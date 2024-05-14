89dba90348d615817282644a7f7ba9a362cb1f12
Benjamin Holderle <ben.holderle.21@lehre.mosbach.dhbw.de>
2024-05-14 09:57:36 +0200

    added d3-dependency

---
+++ src/app/d3/page.tsx
@@ -0,0 +1,18 @@
+import { Prisma } from "@prisma/client";
+import { db } from "~/server/db";
+import { Diagram } from "./diagram";
+
+export default async function D3() {
+
+    const videos = await db.videos.findMany()
+    
+    return (
+        <div>
+            <h1>D3</h1>
+            {/* <Diagram videos={videos} /> */}
+            {
+                videos.map(video => <p key={video.id}>{video.title}</p>)
+            }
+        </div>
+    );
+}
