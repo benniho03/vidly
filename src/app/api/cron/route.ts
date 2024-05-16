0fb6a86ff1f56a8d58ed6e309bcdf9760b2e8692
Benjamin Holderle <ben.holderle.21@lehre.mosbach.dhbw.de>
2024-05-16 17:31:29 +0200

    responding with videos

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -5,27 +5,33 @@ export async function GET() {
 
     console.warn("Cron job started")
 
-    const result = await getVideos({
-        maxResults: 50,
-        searchTerm: "minecraft"
-    })
+    try {
 
-    console.warn("Cron job got videos")
-
-    if (result.body.videos.length === 0) {
-        return new Response("No videos found", { status: 404 })
+        const result = await getVideos({
+            maxResults: 50,
+            searchTerm: "minecraft"
+        })
+        
+        console.warn("Cron job got videos")
+        
+        if (result.body.videos.length === 0) {
+            return new Response("No videos found", { status: 404 })
+        }
+        
+        const { count } = await db.videos.createMany({
+            data: result.body.videos,
+        })
+        
+        console.warn("Created " + count + " videos")
+        
+        return new Response(JSON.stringify(result), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+    } catch(error) {
+        console.error(error)
+        return new Response(JSON.stringify(error), { status: 500 })
     }
-
-    const { count } = await db.videos.createMany({
-        data: result.body.videos,
-    })
-
-    console.warn("Created " + count + " videos")
-
-    return new Response(JSON.stringify(result), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
 }
