12a66f238cfa06f14aae7c7a851dae0a7b2f3df1
Benjamin Holderle <ben.holderle.21@lehre.mosbach.dhbw.de>
2024-05-16 15:40:33 +0200

    cron job

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -3,10 +3,14 @@ import { db } from "~/server/db"
 
 export async function POST() {
 
+    console.log("Cron job started")
+
     const result = await getVideos({
         maxResults: 50,
         searchTerm: "minecraft"
     })
+
+    console.log("Cron job got videos")
 
     if (result.body.videos.length === 0) {
         return new Response("No videos found", { status: 404 })
