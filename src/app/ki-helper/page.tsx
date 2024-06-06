74ae9fcfd262570188816df3beb70a92ffcbe44d
Jessica Guenther <jessica.guenther@hinterhaushelden.de>
2024-06-06 15:45:16 +0200

    styling sites, footer, menu

---
+++ src/app/ki-helper/page.tsx
@@ -0,0 +1,19 @@
+
+
+export default async function KiHelper() {
+    return (
+        <div>
+            <div className="hero">
+                <img src="/assets/hero-ki.jpg" />
+            </div>
+            <div className="container mx-auto px-20">
+                <div className="background">
+                    <div className="background-bg bg-indigo-950"></div>
+                    <h1>Ki-Helper</h1>
+                    <p>Here you can find all the information you need to know about video uploading.</p>
+                    <button type="button" className="text-white bg-fuchsia-800 hover:bg-fuchsia-500 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 dark:bg-fuchsia-800 dark:hover:bg-fuchsia-500 dark:focus:ring-gray-700 dark:border-gray-700">more</button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "~/styles/globals.css";
+import "~/styles/globals.scss";
 
 import { Inter } from "next/font/google";
 
@@ -19,7 +19,61 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+      </head>
+      <body className={inter.className}>
+        <nav className="bg-neutral-50 bg-opacity-80 border-neutral-50 dark:bg-neutral-50 bg-opacity-70">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src="../assets/logo-vidly.png" className="h-8" alt="Vidly Logo" />
+            </a>
+            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+              </svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border bg-neutral-50 rounded-lg bg-neutral-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-neutral-50 dark:border-gray-700">
+                <li>
+                  <a href="#" className="block py-2 px-3 text-neutral-950 rounded md:text-neutral-950 md:p-0 dark:text-neutral-950 md:dark:text-neutral-950">Home</a>
+                </li>
+                <li>
+                  <a href="/gardening" className="block py-2 px-3 text-neutral-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-400 md:p-0 dark:text-neutral-500 md:dark:hover:text-neutral-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Gardening</a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-3 text-neutral-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-400 md:p-0 dark:text-neutral-500 md:dark:hover:text-neutral-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Research</a>
+                </li>
+                <li>
+                  <a href="/ki-helper" className="block py-2 px-3 text-neutral-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-neutral-400 md:p-0 dark:text-neutral-500 md:dark:hover:text-neutral-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">KI-Helper</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        {children}
+        <footer className="bg-white shadow dark:bg-neutral-50 ">
+          <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+              <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="../assets/logo-vidly.png" className="h-8" alt="Vidly Logo" />
+              </a>
+              <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-neutral-500 sm:mb-0 dark:text-neutral-400">
+                <li>
+                  <a href="#" className="hover:underline me-4 md:me-6">Datenschutz</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline me-4 md:me-6">Impressum</a>
+                </li>
+              </ul>
+            </div>
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span className="block text-sm text-neutral-500 sm:text-center dark:text-neutral-400">© 2024 <a href="#" className="hover:underline">vidly</a>. All Rights Reserved.</span>
+          </div>
+        </footer>
+      </body>
+
+
     </html>
   );
 }
