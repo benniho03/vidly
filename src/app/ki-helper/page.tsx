29901a44b0eb3c3bc4faf0592b0e9cfccad71ac7
Jessica Guenther <jessica.guenther@hinterhaushelden.de>
2024-06-12 15:53:58 +0200

    styling research-site, styling pictures

--- src/app/ki-helper/page.tsx
+++ src/app/ki-helper/page.tsx
@@ -12,6 +12,16 @@ export default async function KiHelper() {
                     <h1>Ki-Helper</h1>
                     <p>Here you can find all the information you need to know about video uploading.</p>
                     <button type="button" className="text-white bg-fuchsia-800 hover:bg-fuchsia-500 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 dark:bg-fuchsia-800 dark:hover:bg-fuchsia-500 dark:focus:ring-gray-700 dark:border-gray-700">more</button>
+                    <div className="pictures flex space-x-4">
+
+                        <div className="picture-one">
+                            <img src="/assets/ki-image-1.jpg" />
+                        </div>
+                        <div className="picture-two">
+                            <img src="/assets/ki-image-2.jpg" />
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
