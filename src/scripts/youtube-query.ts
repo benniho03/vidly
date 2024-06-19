825a2adcaaa2ce0b4d4fa1c4358f0d8038c5de77
Benjamin Holderle <ben.holderle.21@lehre.mosbach.dhbw.de>
2024-06-19 09:37:36 +0200

    script for db

--- src/scripts/youtube-query.ts
+++ src/scripts/youtube-query.ts
@@ -62,7 +62,59 @@ export default async function queryVideos() {
     const { count } = await db.videos.createMany({
         data: videos,
     })
-
     console.log("Created " + count + " videos")
+
+    const channelIds = [...new Set(videos.map(video => video.channel))]
+
+    await createChannelData(channelIds)
+
 }
 await queryVideos()
+
+export async function createChannelData(channelIds: string[]) {
+
+    const channelParams = new URLSearchParams({
+        part: "snippet,statistics,status",
+        id: Array.isArray(channelIds) ? channelIds.join(",") : channelIds,
+        key: process.env.API_KEY_CHANNELS as string
+    })
+
+    const channelResponse = await fetch("https://www.googleapis.com/youtube/v3/channels?" + channelParams)
+
+    if (!channelResponse.ok) {
+        console.error(channelResponse.status, channelResponse.statusText)
+        // console.log(await response.text())
+        throw new Error("Failed to fetch videos",)
+    }
+
+    const data = await channelResponse.json()
+
+    const channelData = data.items.map((item: any) => ({
+        id: item.id,
+        title: item.snippet.title ?? null,
+        thumbnail: item.snippet.thumbnails.default.url ?? null,
+        description: item.snippet.description ?? null,
+        publishedAt: item.snippet.publishedAt ?? null,
+        country: item.snippet.country ?? null,
+        viewCount: Number(item.statistics.viewCount) ?? null,
+        subscriberCount: Number(item.statistics.subscriberCount) ?? null,
+        videoCount: Number(item.statistics.videoCount) ?? null,
+        madeforkids: JSON.stringify(item.status.madeForKids) ?? null
+    }))
+
+    for (const channel of channelData) {
+        const currentChannel = await db.channels.findFirst({
+            where: {
+                id: {
+                    contains: channel.id
+                }
+            }
+        })
+
+        if (currentChannel) return
+
+        await db.channels.create({
+            data: channel
+        })
+    }
+}
