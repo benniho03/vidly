87749eca9b1c969d7d139763504a19f33947292e
Jessica Guenther <jessica.guenther@hinterhaushelden.de>
2024-06-20 10:46:45 +0200

    styling fixes and content

--- src/app/ki-helper/page.tsx
+++ src/app/ki-helper/page.tsx
@@ -11,7 +11,7 @@ export default async function KiHelper() {
                 <div className="background mb-20">
                     <div className="background-bg bg-indigo-950"></div>
                     <h1>Ki-Helper</h1>
-                    <p>Here you can find all the information you need to know about video uploading.</p>
+                    <p>You have created your video. Now you want to know how many likes, comments and views your video will get? Then take a look at our AI Helper! Simply enter your video information, such as title, description and so on, and we'll show you how successful your video can be</p>
                     <button type="button" className="text-white bg-fuchsia-800 hover:bg-fuchsia-500 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 dark:bg-fuchsia-800 dark:hover:bg-fuchsia-500 dark:focus:ring-gray-700 dark:border-gray-700">more</button>
                     <div className="pictures flex space-x-4">
 
