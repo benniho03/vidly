6c19e07660ff5083f6b5be837b3aac053040e6eb
Benjamin Holderle <ben.holderle.21@lehre.mosbach.dhbw.de>
2024-06-28 14:04:11 +0200

    formatted more numbers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
         <div className="background mb-0">
           <div className="background-bg bg-slate-900"></div>
           <h1>THE YOUTUBE ALGORITHM </h1>
-          <h2>EVERYBODY KNOWS HIM</h2>
-          <h2>SOME FEAR HIM</h2>
-          <h2 >HARDLY ANYONE UNDERSTANDS HIM</h2>
+          <h2>KNOWN TO ALL</h2>
+          <h2>FEARED BY MANY</h2>
+          <h2>UNDERSTOOD BY NONE</h2>
           <p className='mt-10'>The profession of content creator has become much more popular in recent years, with many more videos appearing on the YouTube platform every day than just a few years ago. Creators, who earn their living from the platform, are heavily dependent on the clicks on their videos, but due to the increased competition and the algorithm used by YouTube, it is becoming increasingly difficult to get many users interested in your own video.</p>
           <div className="keyfacts">
             <div className="number-facts grid grid-cols-3 gap-4">
@@ -26,11 +26,11 @@ export default function Home() {
               <div className="number-facts-item">
                 <p className="content">{formatNumber(720000)}</p >
                 <p className="content-title">hours</p>
-                <p className='content-small'>Videomaterial per day</p>
+                <p className='content-small'>of video per day</p>
               </div>
               <div className="number-facts-item">
                 <p className="content">${formatNumber(300000000)}</p>
-                <p className='content-small'>were generated in advertising revenue from all YouTube combined in 2021</p>
+                <p className='content-small'>were generated in advertising revenue from all of YouTube combined in 2021</p>
               </div>
             </div>
           </div>
@@ -80,7 +80,7 @@ export default function Home() {
           <div className="background-bg bg-indigo-950"></div>
           <div className="two-column flex space-x-4">
             <div className="column-one">
-              <h2>KI-Helper</h2>
+              <h2>AI-Helper</h2>
               <p>You have created your video. Now you want to know how many likes, comments and views your video will get? Then take a look at our AI Helper! Simply enter your video information, such as title, description and so on, and we'll show you how successful your video can be.</p>
               <a href="/ki-helper">
                 <button type="button" className="text-white bg-fuchsia-800 hover:bg-fuchsia-500 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 dark:bg-fuchsia-800 dark:hover:bg-fuchsia-500 dark:focus:ring-gray-700 dark:border-gray-700">more</button>
