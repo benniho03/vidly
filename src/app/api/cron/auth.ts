ae40b7d3f1a026fac33e5cd90e1cda2bfc77b273
Benjamin Holderle <ben.holderle.21@lehre.mosbach.dhbw.de>
2024-07-01 19:06:51 +0200

    removed cron route

--- src/app/api/cron/auth.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-
-export function authenticateCronJob(req: NextRequest) {
-  const authHeader = req.headers.get('authorization');
-  return {
-    authenticated: authHeader === `Bearer ${process.env.CRON_SECRET}`
-  }
-}
